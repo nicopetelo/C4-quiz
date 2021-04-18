662d632287509686094339b84aac02773e48b5ca
nicopetelo <nicopetelo@gmail.com>
2021-04-18 01:46:47 -0700

    getting the question values saved to a var

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,71 +15,68 @@ let numCorrect = 0;
 let quiz = [
     {
         question: "What's something that ants do that is behavior typically associated with humans?",
-        answers: {
-            a: "Calculus",
-            b: "Stretching when waking up",
-            c: "Putting socks on",
-            d: "Foot racing"
-        }
+        answers: [
+            "Calculus",
+            "Stretching when waking up",
+            "Putting socks on",
+            "Foot racing"]
         // correctAnswer: b
     },
     {
         question: "Question 2",
-        answers: {
-            a: "Right",
-            b: "Wrong",
-            c: "Wrong",
-            d: "Wrong"
-        }
+        answers: [
+            "Right",
+            "Wrong",
+            "Wrong",
+            "Wrong"]
         // correctAnswer: a
     },
     {
         question: "Question 3",
-        answers: {
-            a: "Wrong",
-            b: "Right",
-            c: "Wrong",
-            d: "Wrong"
-        }
+        answers: [
+            "Wrong",
+            "Right",
+            "Wrong",
+            "Wrong"]
         // correctAnswer: b
     },
     {
         question: "Question 4",
-        answers: {
-            a: "Wrong",
-            b: "Wrong",
-            c: "Right",
-            d: "Wrong"
-        }
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Right",
+            "Wrong"]
         // correctAnswer: c
     },
     {
         question: "Question 5",
-        answers: {
-            a: "Wrong",
-            b: "Wrong",
-            c: "Wrong",
-            d: "Right"
-        }
+        answers: [
+            "Wrong",
+            "Wrong",
+            "Wrong",
+            "Right"]
         // correctAnswer: d
     }
 ]
 
 createQuestion = () => {
+
     for (i = 0; i < quiz.length; i++) {
-        question = $("#questions").text(quiz.question[i]);
-        console.log(question);
-        for (i = 0; i < answers.length; i++) {
-            // let choice = $("<button></button>").text(quiz.answers[i]);
-            choice = $("btn1").click(function(){
-                    $("#choices").text(quiz.answers[i]);
-                });
+        selectedQuestion = quiz[i].question;
+        displayedQuestion = (i + 1) + ". " + selectedQuestion;
+        console.log(displayedQuestion);
+        for (j = 0; quiz[i].answers.length; j++) {
+            answerOption = j;
         }
-    }
-    $(question).append("#questions");
-    $(choice).append("#choices");
+        
+        // for (j = 0; j < quiz[i].answers.length; j++) {}
+    };
+    $(".questions").append(displayedQuestion);
+    // $(".choices").append(choice);
 }
+console.log("getting it to display outside log");
 
 createQuestion();
 
-$("#final-score").html() = "Your final score was " + finalScore;
+// $("#final-score").html() = "Your final score was " + finalScore;
