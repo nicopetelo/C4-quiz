6672ac44605a96e78a534fa42d7c27c80def9a50
nicopetelo <nicopetelo@gmail.com>
2021-04-17 23:03:29 -0700

    basic html finished and starting js

---
+++ Assets/js/script.js
@@ -0,0 +1,81 @@
+// const questionsContainer = $("#questions").class();
+// console.log(questionsContainer);
+
+// buttons
+const startButton = $("#start-button");
+const submitInitials = $("#submit-initials");
+const goBackButton = $("#go-back");
+const clearHighScores = $("#clear-scores");
+
+let finalScore = 0;
+let numCorrect = 0;
+
+
+
+let quiz = [
+    {
+        question: "What's something that ants do that is behavior typically associated with humans?",
+        answers: {
+            a: "Calculus",
+            b: "Stretching when waking up",
+            c: "Putting socks on",
+            d: "Foot racing"
+        }
+        // correctAnswer: b
+    },
+    {
+        question: "Question 2",
+        answers: {
+            a: "Right",
+            b: "Wrong",
+            c: "Wrong",
+            d: "Wrong"
+        }
+        // correctAnswer: a
+    },
+    {
+        question: "Question 3",
+        answers: {
+            a: "Wrong",
+            b: "Right",
+            c: "Wrong",
+            d: "Wrong"
+        }
+        // correctAnswer: b
+    },
+    {
+        question: "Question 4",
+        answers: {
+            a: "Wrong",
+            b: "Wrong",
+            c: "Right",
+            d: "Wrong"
+        }
+        // correctAnswer: c
+    },
+    {
+        question: "Question 5",
+        answers: {
+            a: "Wrong",
+            b: "Wrong",
+            c: "Wrong",
+            d: "Right"
+        }
+        // correctAnswer: d
+    },
+]
+
+createQuestion = () => {
+    for (i = 0; i < quiz.length; i++) {
+        let question = $("<h2></h2>").text(quiz.questions[i]);
+        $(question).append("#questions");
+        for (i = 0; i < answers.length; i++) {
+            let choice = $("<button></button>").text(quiz.answers[i]);
+            $(choice).append("#choices");
+        }
+    }
+}
+
+createQuestion();
+
+$("#final-score").html() = "Your final score was " + finalScore;
